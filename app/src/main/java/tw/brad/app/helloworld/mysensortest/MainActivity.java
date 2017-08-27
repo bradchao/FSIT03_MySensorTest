3e2fe6237f36bf4c1425ca91cd8ff05fe4e4ccd9
Chien <chien@gmail.com>
2017-08-27 15:27:31 +0800

    v2

--- app/src/main/java/tw/brad/app/helloworld/mysensortest/MainActivity.java
+++ app/src/main/java/tw/brad/app/helloworld/mysensortest/MainActivity.java
@@ -1,5 +1,8 @@
 package tw.brad.app.helloworld.mysensortest;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener2;
@@ -7,6 +10,8 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor sensor;
     private MySensorListener listener;
+    private TextView x, y,z;
+    private MyView myView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        myView = new MyView();
+        setContentView(myView);
+
+//        x = (TextView)findViewById(R.id.x);
+//        y = (TextView)findViewById(R.id.y);
+//        z = (TextView)findViewById(R.id.z);
+
 
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
@@ -32,13 +45,12 @@ public class MainActivity extends AppCompatActivity {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         listener = new MySensorListener();
 
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
@@ -55,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
+            float[] values = sensorEvent.values;
+
+//            x.setText("X:" + (int)(values[0]*100));
+//            y.setText("Y:" + (int)(values[1]*100));
+//            z.setText("Z:" + (int)(values[2]*100));
+            myView.setXY(values[0], values[1]);
+
 
         }
 
@@ -63,5 +82,47 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    private class MyView extends View {
+        private boolean isInit;
+        private float viewW, viewH, ballX, ballY;
+        private Paint paint, paintAxis;
+
+        MyView(){
+            super(MainActivity.this);
+            setBackgroundColor(Color.BLACK);
+            paint = new Paint();
+            paint.setColor(Color.YELLOW);
+            paintAxis = new Paint();
+            paintAxis.setColor(Color.RED);
+            paintAxis.setStrokeWidth(2);
+
+        }
+
+        private void init(){
+            isInit = true;
+            viewW = getWidth(); viewH = getHeight();
+            ballX = viewW / 2f; ballY = viewH / 2f;
+        }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (!isInit) init();
+
+            canvas.drawCircle(ballX, ballY, 80, paint);
+            canvas.drawLine(0,viewH/2f,viewW, viewH/2f, paintAxis);
+            canvas.drawLine(viewW/2f, 0, viewW/2f, viewH, paintAxis);
+
+        }
+
+        void setXY(float x, float y){
+            ballX = viewW/2f + (x * viewW/(2f * 9.8f));
+            ballY = viewH/2f + (y * viewH/(2f * 9.8f));
+            invalidate();
+        }
+
+
+    }
+
 
 }
